b8d3a04456994640dab394db698bf3eee9121255
Edward1yadidLevi <142442624+Edward1yadidLevi@users.noreply.github.com>
2023-09-20 19:36:34 -0700

    .

--- script.js
+++ script.js
@@ -268,20 +268,6 @@ const arraySong = [
   "We don't need no education",
   "We don't need no thought control",
   "No dark sarcasm in the classroom",
-  "Teacher, leave them kids alone",
-  "Hey, teacher, leave them kids alone",
-  "All in all, it's just another brick in the wall",
-  "All in all, you're just another brick in the wall",
-  "We don't need no education",
-  "We don't need no thought control",
-  "No dark sarcasm in the classroom",
-  "Teachers, leave them kids alone",
-  "Hey, teacher, leave us kids alone",
-  "All in all, you're just another brick in the wall",
-  "All in all, you're just another brick in the wall",
-  "If you don't eat yer meat, you can't have any pudding",
-  "How can you have any pudding if you don't eat yer meat?",
-  "You! Yes, you behind the bike stands",
 ];
 
 const divSong = document.createElement("div");
@@ -292,7 +278,7 @@ let line = document.createElement("li");
 divSong.appendChild(song);
 function drawSong(score, arraySong) {
   if (score == 17) {
-    for (i = 0; i < 17; i++) {
+    for (i = 0; i < 3; i++) {
       line.innerHTML += `<li class="lyricsLine">${arraySong[i]}</li>`;
 
       song.appendChild(line);
